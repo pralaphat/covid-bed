3f528489687b8c8816fb67996d1f647f99a0f61e
pralaphat <pralaphat.w@gmail.com>
2021-11-09 14:33:13 +0700

    initial project

---
+++ src/views/Register.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="regis">
+      <div class="hero-body m-6">
+        <Regis />
+      </div>
+  </div>
+</template>
+<script>
+import Regis from '../components/RegisterComponent.vue'
+export default {
+  name: 'regis',
+  components: {
+    Regis
+  }  
+}
+
+</script>
